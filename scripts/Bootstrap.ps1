a1a18f033093c29ceac10254bd5bb680a5e7b850
Jason Chin <chin.subscriptions@gmail.com>
2018-07-06 09:09:32 +1000

    Added commands to uninstall WIndows 10 packages which are not required to address "An App Default Was Reset" Error on Windows 10 after windows update. (https://www.drivethelife.com/windows-10/an-app-default-was-reset-error-windows-10.html)

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -1,7 +1,16 @@
 
 # Resize the disk partition
 $maxPartitionSize = (Get-PartitionSupportedSize -DriveLetter c).sizeMax
-Resize-Partition -DriveLetter c -Size $maxPartitionSize
+Resize-Partition -DriveLetter c -Size $maxPartitionSize -ErrorAction Continue
 
 # Set the timezone
 Set-TimeZone "AUS Eastern Standard Time"
+
+# Fix "An App Default Was Reset" Error on Windows 10
+# https://www.drivethelife.com/windows-10/an-app-default-was-reset-error-windows-10.html
+get-appxpackage *zunevideo* | remove-appxpackage
+get-appxpackage *zunemusic* | remove-appxpackage
+get-appxpackage *photos* | remove-appxpackage
+get-appxpackage *windowsstore* | remove-appxpackage
+get-appxpackage *windowsphone* | remove-appxpackage
+get-appxpackage *xboxapp* | remove-appxpackage
