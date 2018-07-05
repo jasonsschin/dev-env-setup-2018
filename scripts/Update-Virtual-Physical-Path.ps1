d289755be76e74b482c278b010fe5b293fc16e8e
Jason Chin <chin.subscriptions@gmail.com>
2018-07-06 09:14:34 +1000

    Created a script to update the IIS virtual directory physical path to work with Parallels Mac shared folders.

---
+++ scripts/Update-Virtual-Physical-Path.ps1
@@ -0,0 +1,45 @@
+﻿<#
+if ((Get-Module "WebAdministration" -ErrorAction SilentlyContinue) -eq $null){
+	Import-Module WebAdministration
+}
+#>
+
+if (Get-Module -ListAvailable -Name WebAdministration){
+    #Write-Host "Module exists"
+} else {
+    Write-Host "Module does not exist"
+    Write-Host "Import WebAdministration"
+    Import-Module WebAdministration
+}
+
+$Websites = Get-ChildItem IIS:\Sites 
+
+
+$apps = Get-WebApplication -Site $Websites.name
+$website = $Websites.name
+
+ foreach($app in $apps){
+    #Write-Host $webvdirectory.path
+    if ($app.PhysicalPath.StartsWith("D:\anz")){
+    $appname = $app.path.Trim('/')
+    $appPath = $app.path
+
+    Write-Host "Application name: $appname"
+    Write-Host "Physical path: " $app.PhysicalPath
+
+    #$test ="IIS:\Sites\$website\$appName"
+    $path = "/system.applicationHost/sites/site[@name='$website' and @id='1']/application[@path='$appPath']/VirtualDirectory[@path='/']"
+
+    Write-Host "App path: $path"
+
+    $updatedPhysicalPath = $app.PhysicalPath
+    $updatedPhysicalPath = $updatedPhysicalPath.Replace("D:\anz", "\\MAC\Projects\anz")
+
+    Write-Host "Updated physical path: $updatedPhysicalPath"
+
+    Write-Host ""
+    Set-WebConfiguration  $path  -Value  @{PhysicalPath=$updatedPhysicalPath}
+
+    }
+
+ }                                    
