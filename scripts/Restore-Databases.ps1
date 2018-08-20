d5ba40caed68b6ed6a28c1616ad0a2d0ef72311d
Jason Chin <chin.subscriptions@gmail.com>
2018-08-20 15:59:12 +1000

    Add Powershell script to update DefaultAppPool identity.

--- scripts/Restore-Databases.ps1
+++ scripts/Restore-Databases.ps1
@@ -15,23 +15,23 @@ Import-Module SqlServer
 $RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Core", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\core.mdf")
 $RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Core_Log", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\core.ldf")
 
-Copy-Item "\\MAC\downloads\Core-20180108-104709.bak" -Destination "c:\tmp\Core-20180108-104709.bak"
-Restore-SqlDatabase -ServerInstance "." -Database "Core" -BackupFile "c:\tmp\Core-20180108-104709.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog)
-Remove-Item "c:\tmp\Core-20180108-104709.bak"
+Copy-Item "\\MAC\downloads\20180710-Core-Master.bak" -Destination "c:\tmp\20180710-Core-Master.bak"
+Restore-SqlDatabase -ServerInstance "." -Database "Core" -BackupFile "c:\tmp\20180710-Core-Master.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog)
+Remove-Item "c:\tmp\20180710-Core-Master.bak"
 
 
 # Restore Fileactive Database
 $RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Fileactive", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\fileactive.mdf")
 $RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Fileactive_Log", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\fileactve.ldf")
 
-Copy-Item "\\MAC\downloads\Fileactive-20180627.bak" -Destination "c:\tmp\Fileactive-20180627.bak"
-Restore-SqlDatabase -ServerInstance "." -Database "Fileactive" -BackupFile "c:\tmp\Fileactive-20180627.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog )
-Remove-Item "c:\tmp\Fileactive-20180627.bak"
+Copy-Item "\\MAC\downloads\20180710-Fileactive-Rel32.bak" -Destination "c:\tmp\20180710-Fileactive-Rel32.bak"
+Restore-SqlDatabase -ServerInstance "." -Database "Fileactive" -BackupFile "c:\tmp\20180710-Fileactive-Rel32.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog )
+Remove-Item "c:\tmp\20180710-Fileactive-Rel32.bak"
 
 # Restore PSI Database
-$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Fileactive", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\psi.mdf")
-$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("Fileactive_Log", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\psi.ldf")
+$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("PSI", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\psi.mdf")
+$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("PSI_Log", "C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\psi.ldf")
 
-Copy-Item "\\MAC\downloads\20180516-psi.bak" -Destination "c:\tmp\20180516-psi.bak"
-Restore-SqlDatabase -ServerInstance "." -Database "PSI" -BackupFile "c:\tmp\20180516-psi.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog )
-Remove-Item "c:\tmp\20180516-psi.bak"
+Copy-Item "\\MAC\downloads\20180710-PSI-Master.bak" -Destination "c:\tmp\20180710-PSI-Master.bak"
+Restore-SqlDatabase -ServerInstance "." -Database "PSI" -BackupFile "c:\tmp\20180710-PSI-Master.bak" -ReplaceDatabase -RelocateFile @($RelocateData,$RelocateLog )
+Remove-Item "c:\tmp\20180710-PSI-Master.bak"
