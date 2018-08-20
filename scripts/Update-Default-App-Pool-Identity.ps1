e13543b26f255f23576b2bf4d65bc27585e4602a
Jason Chin <chin.subscriptions@gmail.com>
2018-08-20 16:10:27 +1000

    Add Powershell script to update DefaultAppPool identity to use vagrant user credentials

---
+++ scripts/Update-Default-App-Pool-Identity.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module WebAdministration
+
+$ap = Get-Item IIS:\AppPools\DefaultAppPool
+
+$ap.processModel.userName = "vagrant"
+$ap.processModel.password = "vagrant"
+$ap.processModel.identityType = 2 # NetworkService https://docs.microsoft.com/en-us/iis/configuration/system.applicationhost/applicationpools/add/processmodel
+$ap | Set-Item
